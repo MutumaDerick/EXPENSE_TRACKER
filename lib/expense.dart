28c62cbe98af0eee7dbda8506dceb3b4afe1992f
Derick Mutuma <pdmutuma@gmail.com>
2025-02-07 18:14:11 +0300

    Added listView builder to display lists dynamically

--- lib/expense.dart
+++ lib/expense.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -8,13 +10,34 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 10.99,
+      date: DateTime.now(),
+      category: Category.others,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 4.59,
+      date: DateTime.now(),
+      category: Category.entertainment,
+    ),
+    Expense(
+      title: 'Rent',
+      amount: 200,
+      date: DateTime.now(),
+      category: Category.bills,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: const [
-        Text('The chart'),
-        Text('Expenses list'),
-      ]),
+      body: Column(children: [
+        const Text('The chart'),
+        Expanded(child: ExpensesList(expenses: _registeredExpenses))
+      ],),
     );
   }
 }
